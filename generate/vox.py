8d8e9c51beea4324deaa026e0c3e173f739fc1ac
Parker Huntington <huntingt@mit.edu>
2019-11-27 20:35:09 -0500

    Updated vox.py to use command line arguments

--- generate/vox.py
+++ generate/vox.py
@@ -3,22 +3,31 @@ sys.path.append('/Users/huntingt/Documents/6.111/py-vox-io')
 
 from pyvox.parser import VoxParser
 from octree import Octree
+from argparse import ArgumentParser
 
-m = VoxParser(sys.argv[1]).parse();
+if __name__ == '__main__':
+    parser = ArgumentParser("Parse a magica voxel file into an octree format")
+    parser.add_argument('input', help="input octree filename")
+    parser.add_argument('size', type=int, help="cells per octree side")
+    parser.add_argument('-oc', help="output .oc file representing the tree")
+    parser.add_argument('-mat', help="output .mat file representing the materials")
+    args = parser.parse_args()
+    
+    m = VoxParser(args.input).parse(); 
+    oc = Octree(args.size)
 
-voxels = m.models[0].voxels
+    for model in m.models:
+        for voxel in model.voxels:
+            oc.set(voxel.x, voxel.y, voxel.z, voxel.c)
 
-oc = Octree(32)
+    if args.oc:
+        with open(args.oc, 'w') as f:
+            f.write(oc.toOC())
 
-for voxel in voxels:
-    oc.set(voxel.y, voxel.z, voxel.x, voxel.c)
+    if args.mat:
+        output = "0x000000\n"
+        for color in m.palette:
+            output += f"0x{color.b:0{2}x}{color.g:0{2}x}{color.r:0{2}x}\n"
 
-# print palette
-if False:
-    output = "0x000000\n"
-    for color in m.palette:
-        output += f"0x{color.b:0{2}x}{color.g:0{2}x}{color.r:0{2}x}\n"
-    print(output)
-
-if True:
-    print(oc.toOC())
+        with open(args.mat, 'w') as f:
+            f.write(output)
